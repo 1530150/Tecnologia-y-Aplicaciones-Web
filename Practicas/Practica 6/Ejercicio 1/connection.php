a9efaec16836bee5b65b0277b9924b5cb182c99a
Brian Elí Becerra Hernández <1530150@upv.edu.mx>
2018-05-12 11:15:36 -0500

    Ejercicio 1 de la práctica 6

--- Practicas/Practica 6/Ejercicio 1/connection.php
+++ Practicas/Practica 6/Ejercicio 1/connection.php
@@ -1,9 +1,10 @@
 <?php
-
+//Datos de la base de datos
 $dsn = 'mysql:dbname=php_sql_course;host=localhost';
 $user = 'root';
 $password = '';
 
+//Se realiza la conexión dentro de un try para atrapar los errores en dado caso de que se generen
 try{
 
 	$pdo = new PDO(	$dsn,
