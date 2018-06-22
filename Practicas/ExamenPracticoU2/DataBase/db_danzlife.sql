8606655462cbe7390e1d3d59605639fb721ef2b0
Brian Becerra <1530150@upv.edu.mx>
2018-06-21 22:32:17 -0500

    Examen práctico U2 - Avance 2

--- Practicas/ExamenPracticoU2/DataBase/db_danzlife.sql
+++ Practicas/ExamenPracticoU2/DataBase/db_danzlife.sql
@@ -1,38 +1,178 @@
-DROP DATABASE IF EXISTS db_danzlife;
-CREATE DATABASE db_danzlife;
-USE db_danzlife;
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 22-06-2018 a las 05:18:15
+-- Versión del servidor: 10.1.32-MariaDB
+-- Versión de PHP: 7.2.5
 
-CREATE TABLE alumnas(
-	id int AUTO_INCREMENT,
-	nombre varchar(100),
-	apellido varchar(100),
-	fecha_nacimiento varchar(30),
-	grupo int,
-	PRIMARY KEY(id)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE grupos(
-	id int AUTO_INCREMENT,
-	nombre varchar(50),
-	PRIMARY KEY(id)
-);
 
-CREATE TABLE pagos(
-	id int AUTO_INCREMENT,
-	grupo int,
-	alumna int,
-	nombre_mama varchar(100),
-	apellido_mama varchar(100),
-	fecha_pago varchar(30),
-	folio varchar(10),
-	imagen_folio varchar(300),
-	PRIMARY KEY(id),
-	FOREIGN KEY(alumna) REFERENCES alumnas(id),
-	FOREIGN KEY(grupo) REFERENCES grupos(id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE usuarios(
-	nombre varchar(20),
-	password varchar(20),
-	PRIMARY KEY(nombre)
-);
+--
+-- Base de datos: `db_danzlife`
+--
+CREATE DATABASE IF NOT EXISTS `db_danzlife` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `db_danzlife`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnas`
+--
+
+CREATE TABLE `alumnas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `apellido` varchar(100) DEFAULT NULL,
+  `fecha_nacimiento` varchar(30) DEFAULT NULL,
+  `grupo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `alumnas`
+--
+
+INSERT INTO `alumnas` (`id`, `nombre`, `apellido`, `fecha_nacimiento`, `grupo`) VALUES
+(3, 'María', 'Del Angel', '2018-06-17', 2),
+(4, 'Regina', 'Montero', '2018-06-16', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupos`
+--
+
+CREATE TABLE `grupos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `grupos`
+--
+
+INSERT INTO `grupos` (`id`, `nombre`) VALUES
+(2, '1A');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pagos`
+--
+
+CREATE TABLE `pagos` (
+  `id` int(11) NOT NULL,
+  `grupo` int(11) DEFAULT NULL,
+  `alumna` int(11) DEFAULT NULL,
+  `nombre_mama` varchar(100) DEFAULT NULL,
+  `apellido_mama` varchar(100) DEFAULT NULL,
+  `fecha_pago` varchar(30) DEFAULT NULL,
+  `fecha_envio` varchar(30) NOT NULL,
+  `folio` varchar(10) DEFAULT NULL,
+  `imagen_folio` varchar(300) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `pagos`
+--
+
+INSERT INTO `pagos` (`id`, `grupo`, `alumna`, `nombre_mama`, `apellido_mama`, `fecha_pago`, `fecha_envio`, `folio`, `imagen_folio`) VALUES
+(3, 2, 3, 'Pedra', 'Gonzales', '2018-06-21', '21-06-2018 22:35:43', '3298', 'ruta'),
+(4, 2, 4, 'Rumina', 'Pérez', '2018-06-15', '21-06-2018 22:41:06', '93090', 'ruta'),
+(5, 2, 3, 'La Mamá', 'De María', '2018-06-10', '22-06-2018 03:48:00', '393', '/opt/lampp/htdocs/tecnologia-y-aplicaciones-web/danzlife/public/img/peras.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `nombre` varchar(20) NOT NULL,
+  `password` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`nombre`, `password`) VALUES
+('admin', '21232f297a57a5a743894a0e4a801fc3');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumnas`
+--
+ALTER TABLE `alumnas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `grupos`
+--
+ALTER TABLE `grupos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `alumna` (`alumna`),
+  ADD KEY `grupo` (`grupo`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`nombre`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumnas`
+--
+ALTER TABLE `alumnas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `grupos`
+--
+ALTER TABLE `grupos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  ADD CONSTRAINT `pagos_ibfk_1` FOREIGN KEY (`alumna`) REFERENCES `alumnas` (`id`),
+  ADD CONSTRAINT `pagos_ibfk_2` FOREIGN KEY (`grupo`) REFERENCES `grupos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
